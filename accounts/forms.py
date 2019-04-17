c9e66538cef93ae79c09e075a742541c9267c204
withyeah <areygim@gmail.com>
2019-04-17 04:12:11 +0000

    08_Auth update delete && password update

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth import get_user_model
+
+class UserCustomChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email',]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from .forms import UserCustomChangeForm
 
 
 # Create your views here.
@@ -50,3 +53,42 @@ def people(request, username):
         'people': people,
     }
     return render(request, 'accounts/people.html', context)
+
+@login_required    
+def update(request):
+    # POST요청이면 수정 후 form save
+    if request.method == 'POST':
+        form = UserCustomChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('people', request.user.username)
+    # GET요청이면 form 던져주기
+    else:
+        form = UserCustomChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+@login_required    
+def delete(request):
+    if request.method == 'POST':
+        request.user.delete()
+    return redirect('posts:list')
+
+@login_required    
+def password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('posts:list')
+    else:
+        form = PasswordChangeForm(request.POST)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/password.html', context)
+
+    
